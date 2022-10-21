be26d30d51924094102b61f11d295ed8d374e014
vanamalisirisha27 <112234596+vanamalisirisha27@users.noreply.github.com>
2022-10-21 11:01:35 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 class Test{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter a number : ");
     int num=sc.nextInt();
+    System.out.println("Number : "+num);
   }
 }
